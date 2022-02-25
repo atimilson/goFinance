6c55c771c54e156f11baf1db08d9e8b7adecc720
atimilson <atimilson95@gmail.com>
2022-02-25 02:27:40 -0400

    commit metade aula

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Dashboard } from './src/screens/Dashboard';
+import {ThemeProvider} from 'styled-components';
+
+import theme from './src/global/styles/theme'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return(
+  <ThemeProvider theme={theme}>
+   <Dashboard />
+   </ThemeProvider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
