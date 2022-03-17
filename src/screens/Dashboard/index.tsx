fd4c3adb0ea48756c633b532503d53b26a241ec0
atimilson <atimilson95@gmail.com>
2022-03-17 03:01:52 -0400

    inicio aula de cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
-
-import {Container, Title} from './styles';
+import {Container, 
+        Header,
+        UserInfo,
+        User,
+        Photo,
+        UserGreeting,
+        UserName,
+        UserWrapper,
+        Icon
+    } from './styles';
 
 export function Dashboard(){
     return (
         <Container >
-            <Title>Dashboard</Title>
+            <Header>
+              <UserWrapper>
+              
+                    <UserInfo>
+                        <Photo  source={{ uri : 'https://avatars.githubusercontent.com/u/40214696?v=4' }}/>
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Atimilson</UserName>
+                        </User>
+                    </UserInfo>
+                    
+                    <Icon name="power" />
+              </UserWrapper>
+            </Header>
         </Container>
     )
 }
