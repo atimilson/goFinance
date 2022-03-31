9324eb8e56129c12804b964e7ed077313845d665
atimilson <atimilson95@gmail.com>
2022-03-31 01:50:36 -0400

    inicio aula select

---
+++ src/Components/Forms/TransactionButton/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { TouchableOpacityProps } from 'react-native'
+import { Container,Icon ,Title } from './styles';
+
+const icons = {
+  up: 'arrow-up-circle',
+  down: 'arrow-down-circle'  
+}
+interface Props extends TouchableOpacityProps {
+    title: string;
+    type : 'up'| 'down';
+    isActive: boolean;
+}
+
+export function TransactionButton({title,type,isActive, ...rest} : Props) {
+    return(
+        <Container {...rest} type={type}  isActive={isActive}>
+            <Icon 
+            name={icons[type]}
+            type={type} 
+            />
+            <Title>
+                {title}
+            </Title>
+        </Container>
+    )
+}
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,10 +1,17 @@
-import React from "react";
-import { Container, Header, Title , Form , Fields } from "./style";
+import React, {useState} from "react";
+import { Container, Header, Title , Form , Fields, TransactionsTypes } from "./style";
 
 import { Input } from "../../Components/Forms/Input";
 import { Button } from "../../Components/Forms/Button";
+import { TransactionButton } from "../../Components/Forms/TransactionButton";
 
 export function Register(){
+    const [transactionType, setTransactionType] = useState('');
+
+    function handleTransactionSelect(type : 'up'|'down'){
+      setTransactionType(type);   
+      console.log(transactionType);
+    }
     return(
       <Container >
           <Header>
@@ -18,6 +25,20 @@ export function Register(){
                 <Input
                 placeholder="Preço"
                 />
+                <TransactionsTypes>
+                  <TransactionButton 
+                    title="Income" 
+                    type="up" 
+                    onPress={()=>handleTransactionSelect('up')}
+                    isActive={transactionType === 'up'}
+                  />
+                  <TransactionButton 
+                    title="Income" 
+                    type="down"
+                    onPress={()=>handleTransactionSelect('down')}
+                    isActive={transactionType === 'down'}
+                  />
+                </TransactionsTypes>
             </Fields>
             <Button title="Cadastro"/>
           </Form>
